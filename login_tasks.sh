2787b3e0756a726d32033db4ca196dd971e980a0
DAK <46497387+DAK404@users.noreply.github.com>
2024-07-19 15:05:49 +0530

    Update login_tasks.sh

--- login_tasks.sh
+++ login_tasks.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+############################################################
+# Login Tasks Script
+# Version 1.0
+# Date Modified: 19-July-2024
+#
+# --- CHANGELOG ---
+#
+# 1.0 (19-July-2024):
+#    * Bump version to 1.0
+############################################################
+
 #remove the edge gpucache directory first
 rm -r ../.config/microsoft-edge/Default/GPUCache
 
