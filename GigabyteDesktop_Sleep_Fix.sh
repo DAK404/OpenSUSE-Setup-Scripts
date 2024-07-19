3db790ddba77806a7b5d85d593ecc738ce37c0bf
DAK <46497387+DAK404@users.noreply.github.com>
2024-07-19 15:05:03 +0530

    Update GigabyteDesktop_Sleep_Fix.sh

--- GigabyteDesktop_Sleep_Fix.sh
+++ GigabyteDesktop_Sleep_Fix.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+############################################################
+# Sleep/Wakeup Fix For Gigabyte Motherboards
+# Version 1.0
+# Date Modified: 19-July-2024
+#
+# --- CHANGELOG ---
+#
+# 1.0 (19-July-2024):
+#    * Bump version to 1.0
+############################################################
+
 sudo cat > /etc/systemd/system/biosWakeupWorkaround.service << EOL
 [Unit]
 Description=Workaround for Gigabyte BIOS sleep/wakeup bug
