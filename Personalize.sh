68642d6be541d08060c3bddd6635a32025c72854
DAK <46497387+DAK404@users.noreply.github.com>
2024-09-03 21:59:17 +0530

    Update Personalize.sh

--- Personalize.sh
+++ Personalize.sh
@@ -26,7 +26,7 @@ unzip ./Colloid-kde-main.zip
 # Install the KDE and Kvantum theme for ALL users (remove sudo if you want to install to current user only)
 sudo sh ./Colloid-kde-main/install.sh
 # Delete the directory to save space
-rm -r ./Colloid-kde-main
+rm -rf ./Colloid-kde-main
 # Delete the zip file too
 rm Colloid-kde-main.zip
 
@@ -38,9 +38,21 @@ unzip ./Colloid-gtk-theme-main.zip
 # Install the KDE and Kvantum theme
 sudo sh ./Colloid-gtk-theme-main/install.sh
 # Delete the directory to save space
-rm -r ./Colloid-gtk-theme-main
+rm -rf ./Colloid-gtk-theme-main
 # Delete the zip file too
 rm Colloid-gtk-theme-main.zip
+
+echo "[ INFORMATION ] Installing: Colloid Icon Theme"
+# Download the Colloid Icon Theme (Thanks Vinceliuice!)
+curl -LJO https://github.com/vinceliuice/Colloid-icon-theme/archive/refs/heads/main.zip
+# Unzip the dowloaded file
+unzip ./Colloid-icon-theme-main.zip
+# Install the KDE and Kvantum theme
+sudo sh ./Colloid-icon-theme-main/install.sh
+# Delete the directory to save space
+rm -rf ./Colloid-icon-theme-main
+# Delete the zip file too
+rm Colloid-icon-theme-main.zip
 
 
 echo "[ INFORMATION ] Installing: Custom Wallpapers"
