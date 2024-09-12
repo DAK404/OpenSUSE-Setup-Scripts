c1bb3d037589b30c175e999163f05ff6829ea08d
DAK <46497387+DAK404@users.noreply.github.com>
2024-09-13 00:44:11 +0530

    Update Personalize.sh

--- Personalize.sh
+++ Personalize.sh
@@ -5,6 +5,11 @@
 #
 # --- CHANGELOG ---
 #
+# 1.2 (13-September-2024):
+#    * Add commands to download and install 
+#      Posy's Cursor Set
+#    * Add separators to make the script more readable
+#
 # 1.1 (14-August-2024):
 #    * Remove MacSonoma theme
 #    * Add Colloid Kvantum and GTK themes
@@ -14,9 +19,13 @@
 #    * Initial Commit
 ############################################################
 
+# ---------------------------------------------------------- #
+
 echo "[ INFORMATION ] Installing: Kvantum Manager"
 # Install Kvantum Manager & GTK theme requirements
 sudo zypper in -y kvantum-manager gtk2-engine-murrine sassc
+
+# ---------------------------------------------------------- #
 
 echo "[ INFORMATION ] Installing: KDE & Kvantum Theme"
 # Download the Colloid Theme (Thanks Vinceliuice!)
@@ -27,8 +36,8 @@ unzip ./Colloid-kde-main.zip
 sudo sh ./Colloid-kde-main/install.sh
 # Delete the directory to save space
 rm -rf ./Colloid-kde-main
-# Delete the zip file too
-rm Colloid-kde-main.zip
+
+# ---------------------------------------------------------- #
 
 echo "[ INFORMATION ] Installing: Colloid GTK Theme"
 # Download the Colloid Theme (Thanks Vinceliuice!)
@@ -39,8 +48,8 @@ unzip ./Colloid-gtk-theme-main.zip
 sudo sh ./Colloid-gtk-theme-main/install.sh
 # Delete the directory to save space
 rm -rf ./Colloid-gtk-theme-main
-# Delete the zip file too
-rm Colloid-gtk-theme-main.zip
+
+# ---------------------------------------------------------- #
 
 echo "[ INFORMATION ] Installing: Colloid Icon Theme"
 # Download the Colloid Icon Theme (Thanks Vinceliuice!)
@@ -51,20 +60,46 @@ unzip ./Colloid-icon-theme-main.zip
 sudo sh ./Colloid-icon-theme-main/install.sh
 # Delete the directory to save space
 rm -rf ./Colloid-icon-theme-main
-# Delete the zip file too
-rm Colloid-icon-theme-main.zip
 
+# ---------------------------------------------------------- #
+
+echo "[ INFORMATION ] Installing: Posy's Cursor Set"
+# Download the Posy's Cursor Set (Thanks Michel "Posy" de Boer and Simtrani for porting this to Linux!)
+curl -LJO https://github.com/simtrami/posy-improved-cursor-linux/archive/refs/heads/main.zip
+# Unzip the dowloaded file
+unzip ./posy-improved-cursor-linux-main.zip
+# Clean up unwanted directories and files
+rm -rf ./posy-improved-cursor-linux-main/readme_files
+rm -rf ./posy-improved-cursor-linux-main/README.md
+# Install the cursor sets
+sudo cp -r ./posy-improved-cursor-linux-main/* /usr/share/icons
+# Delete the directory
+rm -rf ./posy-improved-cursor-linux-main/
+
+# ---------------------------------------------------------- #
 
 echo "[ INFORMATION ] Installing: Custom Wallpapers"
 sudo cp ./custom-wallpapers/* /usr/share/wallpapers
 
+# ---------------------------------------------------------- #
+
 echo "[ INFORMATION ] Installing: Sound Theme"
 # Install the custom MacOcean theme (Ocean theme with the Mac startup sound)
 sudo cp -r ./MacOcean /usr/share/sounds
+
+# ---------------------------------------------------------- #
 
 echo "[ INFORMATION ] Installing: Login Task"
 # Create directory '~/.scripts'
 mkdir ~/.scripts
 # Copy the login_tasks.sh file to '~/.scripts/' directory
 cp login_tasks.sh ~/.scripts/
+
+# ---------------------------------------------------------- #
+
+echo "[ INFORMATION ] Cleaning Up ZIP Files"
+# remove all zip files in the directory
+rm -rf ./*.zip
+
+# ---------------------------------------------------------- #
 
