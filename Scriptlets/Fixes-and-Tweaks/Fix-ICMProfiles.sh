3d48f913fdfafde6a2bbefdabde23be800b7ef84
DAK <46497387+DAK404@users.noreply.github.com>
2025-10-06 23:58:05 +0530

    Clean up older contents: Begin Afresh
    As the commit message says...

--- Scriptlets/Fixes-and-Tweaks/Fix-ICMProfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-############################################################
-# Fix: Install ICM Profiles
-############################################################
-
-cp ./color/*.icm /usr/share/color
-
-echo "[ INFORMATION ] ICM Profiles Installed"
